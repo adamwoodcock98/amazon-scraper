24f6d8e2eb1dd22bf3e38c6722da75e0ff4e5337
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-07-05 17:00:37 +0100

    Refactored api key into request query

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.port || 5000;
 
-const apiKey = '8c8723fc5e308c87714c04e501e24295'
-const baseURL = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+const generateScraperUrl = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
 
 app.use(express.json());
 
@@ -16,9 +15,10 @@ app.get('/', (req, res) => {
 // GET Product Details
 app.get('/products/:productId', async (req, res) => {
   const { productId } = req.params;
+  const { api_key } = req.query;
 
   try {
-    const response = await request(`${baseURL}&url=https://www.amazon.com/dp/${productId}`)
+    const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/dp/${productId}`)
 
     res.json(JSON.parse(response));
   } catch(err) {
@@ -30,9 +30,10 @@ app.get('/products/:productId', async (req, res) => {
 // GET Product Reviews
 app.get('/products/:productId/reviews', async (req, res) => {
   const { productId } = req.params;
+  const { api_key } = req.query;
 
   try {
-    const response = await request(`${baseURL}&url=https://www.amazon.com/product-reviews/${productId}`)
+    const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/product-reviews/${productId}`)
 
     res.json(JSON.parse(response));
   } catch(err) {
@@ -44,9 +45,10 @@ app.get('/products/:productId/reviews', async (req, res) => {
 // GET Search Results
 app.get('/search/:searchQuery', async (req, res) => {
   const { searchQuery } = req.params;
+  const { api_key } = req.query;
 
   try {
-    const response = await request(`${baseURL}&url=https://www.amazon.com/s?k=${searchQuery}`)
+    const response = await request(`${generateScraperUrl(api_key)}&url=https://www.amazon.com/s?k=${searchQuery}`)
 
     res.json(JSON.parse(response));
   } catch(err) {
