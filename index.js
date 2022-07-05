f357a6bc2b0e87384f23bfb0a8a0d3f8a94081b1
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-07-05 16:55:48 +0100

    Created search endpoint

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/products/:productId', async (req, res) => {
   const { productId } = req.params;
 
   try {
-    const response = await request(`${baseURL}&url=https://www.amazon.co.uk/dp/${productId}`)
+    const response = await request(`${baseURL}&url=https://www.amazon.com/dp/${productId}`)
 
     res.json(JSON.parse(response));
   } catch(err) {
@@ -32,7 +32,21 @@ app.get('/products/:productId/reviews', async (req, res) => {
   const { productId } = req.params;
 
   try {
-    const response = await request(`${baseURL}&url=https://www.amazon.co.uk/product-reviews/${productId}`)
+    const response = await request(`${baseURL}&url=https://www.amazon.com/product-reviews/${productId}`)
+
+    res.json(JSON.parse(response));
+  } catch(err) {
+    console.log(err.message);
+    res.status(500).send(err)
+  }
+})
+
+// GET Search Results
+app.get('/search/:searchQuery', async (req, res) => {
+  const { searchQuery } = req.params;
+
+  try {
+    const response = await request(`${baseURL}&url=https://www.amazon.com/s?k=${searchQuery}`)
 
     res.json(JSON.parse(response));
   } catch(err) {
