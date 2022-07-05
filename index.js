1aa93d45534ba736243ab6660bdea6efc33152cd
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-07-05 13:17:47 +0100

    Created product info endpoint

--- index.js
+++ index.js
@@ -4,10 +4,27 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.port || 5000;
 
+const apiKey = '8c8723fc5e308c87714c04e501e24295'
+const baseURL = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Amazon Scraper API')
+})
+
+// GET Product Details
+app.get('/products/:productId', async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(`${baseURL}&url=https://www.amazon.co.uk/dp/${productId}`)
+
+    res.json(JSON.parse(response));
+  } catch(err) {
+    console.log(err.message);
+    res.status(500).send(err)
+  }
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
